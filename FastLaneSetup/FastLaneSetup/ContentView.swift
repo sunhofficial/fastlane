109af5412e3dbd9acae76cf6c79f950e45c72d42
Sunho <sunh803@naver.com>
2023-08-09 20:07:34 +0900

    fastlane setting error

---
+++ FastLaneSetup/FastLaneSetup/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FastLaneSetup
+//
+//  Created by 235 on 2023/08/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastLaneSetup/FastLaneSetup/FastLaneSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastLaneSetupApp.swift
+//  FastLaneSetup
+//
+//  Created by 235 on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct FastLaneSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
